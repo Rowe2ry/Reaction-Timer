911962d3b775189e0f9cf1d58602c3d48cc63d31
Chris Rowe <rowe2ry@yahoo.com>
2022-04-30 13:45:45 -0400

    Adding Vue app

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"></Block>
+  <Results v-if="score != null" :score="score"> </Results>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+
+export default {
+  name: 'App',
+  components: {
+      Block,
+      Results
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null
+    }
+  },
+  methods: {
+    start() {
+      this.score = null
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
